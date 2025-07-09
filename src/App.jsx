ee0793d7cf9cf7c5d6f9d9187023a4f3ac0a96ca
yadhukrishnx <yadhukrishnx@gmail.com>
2025-07-09 20:38:56 +0530

    feat(router page):add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './router';  // assuming the filename is router.jsx
 
-import './App.css'
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
-     <h1>Welcome to SmrithiVault</h1>
-    </>
-  )
-}
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/landing/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div style={{ color: 'black' }}>Landing page</div>
+  )
+}
+
+export default Landing  
